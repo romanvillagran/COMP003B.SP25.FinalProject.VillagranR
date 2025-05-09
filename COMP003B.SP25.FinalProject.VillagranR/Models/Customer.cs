4a0387e742a84b9e4c5ccf0bfcb6d3ef4786ea80
cbig3 <romanvillagran386@gmail.com>
2025-05-09 00:15:15 -0700

    added a customer.cs in model folder and also added the way tp book service

---
+++ COMP003B.SP25.FinalProject.VillagranR/Models/Customer.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace COMP003B.SP25.FinalProject.VillagranR.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string Phone { get; set; }
+
+        [StringLength(200)]
+        public string Address { get; set; }
+    }
+}
