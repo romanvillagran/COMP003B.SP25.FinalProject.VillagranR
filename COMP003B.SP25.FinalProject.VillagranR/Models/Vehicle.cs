125ea5a711912ce4c1bb201d4692d6525076cb43
cbig3 <romanvillagran386@gmail.com>
2025-05-09 01:12:49 -0700

    added a vehicle.cs file in models and also added a feacture to add the make, model, year, and license plate #

---
+++ COMP003B.SP25.FinalProject.VillagranR/Models/Vehicle.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace COMP003B.SP25.FinalProject.VillagranR.Models
+{
+    public class Vehicle
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Make { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Model { get; set; }
+
+        [Required]
+        [Range(1900, 2100)]
+        public int Year { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string LicensePlate { get; set; }
+
+        public int CustomerId { get; set; }
+
+        public Customer? Customer { get; set; }
+    }
+}
