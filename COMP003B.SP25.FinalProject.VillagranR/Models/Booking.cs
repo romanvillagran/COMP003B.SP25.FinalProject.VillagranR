7e03ebe4bcd482b9d4bf51a542612235860f998c
cbig3 <romanvillagran386@gmail.com>
2025-05-09 23:47:40 -0700

    added content to booking.cs file

--- COMP003B.SP25.FinalProject.VillagranR/Models/Booking.cs
+++ COMP003B.SP25.FinalProject.VillagranR/Models/Booking.cs
@@ -1,7 +1,18 @@
-﻿namespace COMP003B.SP25.FinalProject.VillagranR.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace COMP003B.SP25.FinalProject.VillagranR.Models
 {
     public class Booking
     {
+        public int Id { get; set; }
 
+        [Required]
+        public DateTime BookingDate { get; set; }
+
+        public int VehicleId { get; set; }
+        public int ServiceId { get; set; }
+
+        public Vehicle? Vehicle { get; set; }
+        public Service? Service { get; set; }
     }
 }
