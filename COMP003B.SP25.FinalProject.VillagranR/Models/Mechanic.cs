2d06c1b4d4ac6c24e6bbd87ac5da05adb19e2532
cbig3 <romanvillagran386@gmail.com>
2025-05-10 00:09:01 -0700

    added the details about the mechanic

--- COMP003B.SP25.FinalProject.VillagranR/Models/Mechanic.cs
+++ COMP003B.SP25.FinalProject.VillagranR/Models/Mechanic.cs
@@ -1,6 +1,29 @@
-﻿namespace COMP003B.SP25.FinalProject.VillagranR.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace COMP003B.SP25.FinalProject.VillagranR.Models
 {
     public class Mechanic
     {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Specialty { get; set; }
+
+        [Required]
+        [Range(0, 50)]
+        public int YearsExperience { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string Phone { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
     }
 }
