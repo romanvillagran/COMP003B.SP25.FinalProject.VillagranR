c33a41ecdf88f624fce29bc5094ca05f31a57833
cbig3 <romanvillagran386@gmail.com>
2025-05-10 01:03:58 -0700

    added all the db to the db context file

--- COMP003B.SP25.FinalProject.VillagranR/Data/ApplicationDbContext.cs
+++ COMP003B.SP25.FinalProject.VillagranR/Data/ApplicationDbContext.cs
@@ -1,9 +1,19 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using COMP003B.SP25.FinalProject.VillagranR.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace COMP003B.SP25.FinalProject.VillagranR.Data
 {
     public class ApplicationDbContext : DbContext 
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+           : base(options)
+        {
+        }
 
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Vehicle> Vehicles { get; set; }
+        public DbSet<Service> Services { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
+        public DbSet<Mechanic> Mechanics { get; set; }
     }
 }
