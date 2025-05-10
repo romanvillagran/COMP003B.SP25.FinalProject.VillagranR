21a84268f555f9203785f25d04d787495fd1b533
cbig3 <romanvillagran386@gmail.com>
2025-05-09 23:28:48 -0700

    added content to the file

--- COMP003B.SP25.FinalProject.VillagranR/Models/Service.cs
+++ COMP003B.SP25.FinalProject.VillagranR/Models/Service.cs
@@ -1,6 +1,25 @@
-﻿namespace COMP003B.SP25.FinalProject.VillagranR.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace COMP003B.SP25.FinalProject.VillagranR.Models
 {
     public class Service
     {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(500)]
+        public string Description { get; set; }
+
+        [Required]
+        [Range(0, 10000)]
+        public decimal Price { get; set; }
+
+        [Required]
+        [Range(0, 24)]
+        public int DurationHours { get; set; }
     }
 }
