68e17c17ef373720daac3ed24426b4641fc59363
cbig3 <romanvillagran386@gmail.com>
2025-05-12 21:51:29 -0700

    Add project files.

---
+++ COMP003B.SP25.FinalProject.VillagranR/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace COMP003B.SP25.FinalProject.VillagranR.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
