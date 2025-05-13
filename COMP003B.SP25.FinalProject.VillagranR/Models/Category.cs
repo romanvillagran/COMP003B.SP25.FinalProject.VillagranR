bea17e46cf921a9889fb3c98c1dfa4ee57953925
cbig3 <romanvillagran386@gmail.com>
2025-05-12 22:04:12 -0700

    added to venue and category

--- COMP003B.SP25.FinalProject.VillagranR/Models/Category.cs
+++ COMP003B.SP25.FinalProject.VillagranR/Models/Category.cs
@@ -1,6 +1,16 @@
-﻿namespace COMP003B.SP25.FinalProject.VillagranR.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace COMP003B.SP25.FinalProject.VillagranR.Models
 {
     public class Category
     {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Category name is required")]
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public List<Event> Events { get; set; }
     }
 }
--- COMP003B.SP25.FinalProject.VillagranR/Models/Venue.cs
+++ COMP003B.SP25.FinalProject.VillagranR/Models/Venue.cs
@@ -1,6 +1,20 @@
-﻿namespace COMP003B.SP25.FinalProject.VillagranR.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace COMP003B.SP25.FinalProject.VillagranR.Models
 {
     public class Venue
     {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Venue name is required")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Address is required")]
+        public string Address { get; set; }
+
+        [Range(1, 10000, ErrorMessage = "Capacity must be 1-10,000")]
+        public int Capacity { get; set; }
+
+        public List<Event> Events { get; set; }
     }
 }
