b8e0af6f354ca10b6eb4ae806140a1d772bede66
cbig3 <romanvillagran386@gmail.com>
2025-05-12 21:57:14 -0700

    added to event

--- COMP003B.SP25.FinalProject.VillagranR/Models/Event.cs
+++ COMP003B.SP25.FinalProject.VillagranR/Models/Event.cs
@@ -1,6 +1,26 @@
-﻿namespace COMP003B.SP25.FinalProject.VillagranR.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace COMP003B.SP25.FinalProject.VillagranR.Models
 {
     public class Event
     {
+        public int Id { get; set; }
+
+        [Required, StringLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        public DateTime Date { get; set; }
+
+        [Required, StringLength(200)]
+        public string Description { get; set; }
+
+        public int CategoryId { get; set; }
+        public Category? Category { get; set; }
+
+        public int VenueId { get; set; }
+        public Venue? Venue { get; set; }
+
+        public ICollection<Registration>? Registrations { get; set; }
     }
 }
