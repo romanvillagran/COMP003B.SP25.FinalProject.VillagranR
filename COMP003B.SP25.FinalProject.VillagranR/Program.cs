4fa65c5e54130d9622174de35d513b630e80999a
cbig3 <romanvillagran386@gmail.com>
2025-05-12 22:15:59 -0700

    added the json and db context to programs

--- COMP003B.SP25.FinalProject.VillagranR/Program.cs
+++ COMP003B.SP25.FinalProject.VillagranR/Program.cs
@@ -1,3 +1,6 @@
+using COMP003B.SP25.FinalProject.VillagranR.Models;
+using Microsoft.EntityFrameworkCore;
+
 namespace COMP003B.SP25.FinalProject.VillagranR
 {
     public class Program
@@ -8,6 +11,9 @@ namespace COMP003B.SP25.FinalProject.VillagranR
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
             var app = builder.Build();
 
