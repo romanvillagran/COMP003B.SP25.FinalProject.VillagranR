b0b6c5c6264b46c355b246fd8a0aad23142da224
cbig3 <romanvillagran386@gmail.com>
2025-05-12 22:08:55 -0700

    added content to the db context file

--- COMP003B.SP25.FinalProject.VillagranR/Models/ApplicationDbContext.cs
+++ COMP003B.SP25.FinalProject.VillagranR/Models/ApplicationDbContext.cs
@@ -1,6 +1,18 @@
-﻿namespace COMP003B.SP25.FinalProject.VillagranR.Models
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace COMP003B.SP25.FinalProject.VillagranR.Models
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Event> Events { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Registration> Registrations { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Venue> Venues { get; set; }
     }
 }
