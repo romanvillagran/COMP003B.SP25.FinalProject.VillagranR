eee70114697d534b2d34c334f5c31ec1155c6a12
cbig3 <romanvillagran386@gmail.com>
2025-05-12 22:01:28 -0700

    added to both registration and student

--- COMP003B.SP25.FinalProject.VillagranR/Models/Registration.cs
+++ COMP003B.SP25.FinalProject.VillagranR/Models/Registration.cs
@@ -2,5 +2,22 @@
 {
     public class Registration
     {
+    
+        public int Id { get; set; }
+
+        
+        public int EventId { get; set; }
+
+       
+        public int StudentId { get; set; }
+
+        
+        public DateTime RegistrationDate { get; set; }
+
+        
+        public Event Event { get; set; }
+
+
+        public Student Student { get; set; }
     }
 }
--- COMP003B.SP25.FinalProject.VillagranR/Models/Student.cs
+++ COMP003B.SP25.FinalProject.VillagranR/Models/Student.cs
@@ -1,6 +1,31 @@
-﻿namespace COMP003B.SP25.FinalProject.VillagranR.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace COMP003B.SP25.FinalProject.VillagranR.Models
 {
     public class Student
     {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage = "First name cannot exceed 50 characters")]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage = "Last name cannot exceed 50 characters")]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [Phone]
+        [Display(Name = "Phone Number")]
+        public string PhoneNumber { get; set; }
+
+       
+        public virtual ICollection<Registration>? Registrations { get; set; }
     }
 }
